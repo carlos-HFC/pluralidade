f56bf018c997b6b769b513adbdbaec85bb8b9652
Carlos Faustino <chfcchfc96@gmail.com>
2021-10-27 11:22:31 -0300

    Validar token JWT

---
+++ backend/src/common/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, HttpException, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector
+  ) { }
+
+  async canActivate(ctx: ExecutionContext) {
+    const roles = this.reflector.get<string[]>('roles', ctx.getHandler());
+
+    if (!roles) return true;
+
+    const { user } = ctx.switchToHttp().getRequest() as Request;
+
+    const hasRole = () => roles.find(item => item.toLowerCase() === user.role.name.toLowerCase());
+
+    if (!hasRole()) throw new HttpException("Você não tem permissão", 403);
+
+    return user && true;
+  }
+}
