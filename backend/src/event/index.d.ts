c3e80a62d517695d90b2d4ce2029257cef42c8f6
Carlos Faustino <chfcchfc96@gmail.com>
2021-10-27 11:20:34 -0300

    .

--- backend/src/event/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ICreateEvent {
-  title: string;
-  description: string;
-  date: string;
-  initHour: string;
-  endHour: string;
-}
-
-export interface IUpdateEvent extends Partial<ICreateEvent> { }
