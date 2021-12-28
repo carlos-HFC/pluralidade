587081f7a102770c89527a461bb44244259ae425
Carlos Faustino <chfcchfc96@gmail.com>
2021-12-28 18:04:44 -0300

    CSS variables

---
+++ frontend/src/styles/variables.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+  purple: '#7946bf',
+  purpleLight: '#5850da',
+  pink: '#93278f',
+  pinkLight: '#ff7bac',
+  green: '#006837',
+  greenLight: '#abc434',
+  orange: '#f15a24',
+  orangeLight: '#f7931e',
+  gray: '#4d4d4d',
+  turquoise: '#00a99d',
+  black: '#212121',
+  red: '#ca0809',
+  white: '#fff',
+  facebook: '#3b5998',
+  twitter: '#61c5ec',
+  instagram: '#8a3ab9',
+}
