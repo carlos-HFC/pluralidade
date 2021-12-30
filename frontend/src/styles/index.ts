e601875c1480eaf062f81f206095bf36192a1d00
Carlos Faustino <chfcchfc96@gmail.com>
2021-12-30 17:45:49 -0300

    Centralizar estilização no styled-components

--- frontend/src/styles/index.ts
+++ frontend/src/styles/index.ts
@@ -1,46 +1,60 @@
 import { darken } from 'polished';
 import { createGlobalStyle } from 'styled-components';
 
+import { COLORS } from "./variables";
+
 export const GlobalStyle = createGlobalStyle`
   * {
     transition: color .3s, background-color .3s;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    line-height: 1.5;
+  }
+
+  canvas {
+    display: none;
+  }
+
+  ::-webkit-scrollbar {
+    width: 0.5rem;
   }
 
   ::-webkit-scrollbar-thumb {
     background: ${props => props.theme.buttons.primary};
+    border-radius: 0.5rem;
+    border: 0.5px solid #eee;
     
     &:hover {
       background: ${props => darken(.05, props.theme.buttons.primary)};
     }
   }
 
+  ::-webkit-scrollbar-track-piece {
+    background: #eee;
+    box-shadow: 0 0 1.5px ${COLORS.black};
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
   body {
     background: ${props => props.theme.background};
     color: ${props => props.theme.text};
+    font-size: 1rem;
   }
 
-  .header {
-    background: ${props => props.theme.header};
-    color: ${props => props.theme.text};
-
-    .menu-list {
-      background: ${props => props.theme.header};
-    }
-  }
-
-  .login, #signup {
+  .login {
+    padding: 40px;
+    border-radius: 1rem !important;
+    width: 100% !important;
     background: ${props => props.theme.login.background};
 
     h3, a, svg {
       color: ${props => props.theme.text}
-    }
-  }
-
-  .footer {
-    background: ${props => props.theme.footer};
-
-    *, a {
-      color: ${props => props.theme.text} !important;
     }
   }
   
