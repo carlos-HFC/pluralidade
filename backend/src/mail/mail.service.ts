3f29d22c70e16859f1ce45e9766e5349d0469284
Carlos Faustino <chfcchfc96@gmail.com>
2022-03-07 17:47:48 -0300

    Mudanças nas funções, links estarão com query string

--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -1,9 +1,10 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { InjectSendGrid, SendGridService } from '@ntegral/nestjs-sendgrid';
-import { format, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
 import { Course } from '../course/course.model';
+import { Solicitation } from '../solicitation/solicitation.model';
 import { User } from '../user/user.model';
 
 @Injectable()
@@ -13,7 +14,7 @@ export class MailService {
     private client: SendGridService
   ) { }
 
-  async storeUser(user: User) {
+  async newUser(user: User) {
     const envelope = {
       from: "NoReply <chfcchfc@hotmail.com>",
       templateId: "d-10f2eb9ca24e40a6b4e785fa729ab945",
@@ -26,9 +27,9 @@ export class MailService {
           {
             to: user.email,
             dynamicTemplateData: {
-              subject: "Bem-vindo",
+              subject: "Bem-vindo(a) ao Instituto Pluralidade!!",
               name: user.name,
-              link: "http://localhost:3000"
+              link: `http://localhost:3000/verifyRegister?token=${user.tokenEmailVerification}&email=${user.email}`
             },
           },
         ],
@@ -38,7 +39,32 @@ export class MailService {
     }
   }
 
-  async forgotPass(user: User, token: string) {
+  async updateEmail(user: User) {
+    const envelope = {
+      from: "NoReply <chfcchfc@hotmail.com>",
+      templateId: "",
+    };
+
+    try {
+      await this.client.send({
+        ...envelope,
+        personalizations: [
+          {
+            to: user.email,
+            dynamicTemplateData: {
+              subject: "Confirmação de e-mail",
+              name: user.name,
+              link: `http://localhost:3000/verifyRegister?token=${user.tokenEmailVerification}&email=${user.email}`
+            },
+          },
+        ],
+      });
+    } catch (error) {
+      throw new HttpException(error, 400);
+    }
+  }
+
+  async forgotPassword(user: User) {
     const envelope = {
       from: "NoReply <chfcchfc@hotmail.com>",
       templateId: "d-b0828206bd434efcbe876c8ad8585c3a"
@@ -53,8 +79,7 @@ export class MailService {
             dynamicTemplateData: {
               subject: "Esqueceu a senha?",
               name: user.name,
-              link: "http://localhost:3000",
-              token
+              link: `http://localhost:3000/updatePassword?token=${user.tokenResetPassword}&email=${user.email}`
             },
           },
         ],
@@ -64,7 +89,7 @@ export class MailService {
     }
   }
 
-  async storeSolicitation(user: User, protocol: number) {
+  async sendSolicitation(solicitation: Solicitation) {
     const envelope = {
       from: "NoReply <chfcchfc@hotmail.com>",
       templateId: "d-eef0c36e5c1f43e28ff2253c805e9d56"
@@ -75,11 +100,10 @@ export class MailService {
         ...envelope,
         personalizations: [
           {
-            to: user.email,
+            to: solicitation.email,
             dynamicTemplateData: {
-              subject: `Abertura de Solicitação #${protocol}`,
-              name: user.name,
-              protocol
+              subject: "Sua solicitação foi enviada com sucesso!!",
+              name: solicitation.name
             },
           },
         ],
@@ -95,41 +119,41 @@ export class MailService {
       templateId: "d-c94d1642bd2d46178dedb69c61b20fe5"
     };
 
-    const hour = (period: string) => ({
-      manha: {
+    const hour = {
+      'M': {
         initHour: '08:00',
         endHour: '11:00',
       },
-      tarde: {
+      'T': {
         initHour: '13:00',
         endHour: '16:00',
       },
-      noite: {
+      'N': {
         initHour: '18:00',
         endHour: '21:00',
       },
-    })[period.toLowerCase().normalize().trim()];
+    }
 
-    // const initDate = format(parseISO(course.initDate), "dd 'de' MMMM 'de' yyyy", { locale: ptBR });
+    const initDate = format(course.initDate, "dd 'de' MMMM 'de' yyyy", { locale: ptBR });
 
     try {
-      // await this.client.send({
-      //   ...envelope,
-      //   personalizations: [
-      //     {
-      //       to: user.email,
-      //       dynamicTemplateData: {
-      //         subject: `Inscrição no curso ${course.name}`,
-      //         name: user.name,
-      //         courseName: course.name,
-      //         initDate,
-      //         period: course.period,
-      //         initHour: hour(course.period).initHour,
-      //         endHour: hour(course.period).endHour
-      //       },
-      //     },
-      //   ],
-      // });
+      await this.client.send({
+        ...envelope,
+        personalizations: [
+          {
+            to: user.email,
+            dynamicTemplateData: {
+              subject: `Inscrição no curso ${course.name} efetuada com sucesso!!`,
+              name: user.name,
+              courseName: course.name,
+              initDate,
+              period: course.period,
+              initHour: hour[course.period].initHour,
+              endHour: hour[course.period].endHour
+            },
+          },
+        ],
+      });
     } catch (error) {
       throw new HttpException(error, 400);
     }
