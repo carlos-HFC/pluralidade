08a3f83758fe5749f4de9bb16e100ec346c49a54
Carlos <carlos.faustino@bcrcx.com>
2022-03-17 08:52:12 -0300

    Alterar texto de acordo com a página

--- frontend/src/components/Accessibility/index.tsx
+++ frontend/src/components/Accessibility/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useTheme } from 'styled-components';
 
 import * as S from './style';
@@ -9,10 +11,15 @@ type AccessibilityProps = {
 export function Accessibility({ handleTheme }: AccessibilityProps) {
   const { name } = useTheme();
 
+  const { pathname } = useLocation();
+
   return (
     <S.AccessibilityWrapper>
       <S.Container>
-        <S.Link href="#menu" title="Menu">Menu</S.Link>
+        {pathname === "/register"
+          ? <Link to="/" title="Home">Home</Link>
+          : <S.Link href="#menu" title="Menu">Menu</S.Link>}
+
         <S.Link href="#main" title="Conteúdo">Conteúdo</S.Link>
         <S.Switch title="Mudar tema">
           <input type="checkbox" onClick={handleTheme} defaultChecked={name === 'dark'} />
