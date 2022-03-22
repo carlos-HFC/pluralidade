b2ef95ae038024b78dc3ecd11e1abf2802171e1f
Carlos Faustino <chfcchfc96@gmail.com>
2022-03-22 16:51:57 -0300

    Alt nas imagens

--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -1,23 +1,22 @@
 import { useEffect, useRef, useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
-import Img1 from '../../assets/img1.jpg';
-import Img2 from '../../assets/img2.jpg';
-import Img3 from '../../assets/img3.jpg';
+import Img1 from '../../assets/img1.webp';
+import Img2 from '../../assets/img2.webp';
+import Img3 from '../../assets/img3.webp';
 import Img4 from '../../assets/img4.jpg';
 import Img5 from '../../assets/img5.jpg';
-import Img6 from '../../assets/img6.jpg';
 
 import * as S from './style';
 
 const images = [
   {
     img: Img1,
-    alt: "Uma sala de aula cheia de alunos sentados em suas carteiras. Um professor, a frente da sala, está dando uma aula de matemática. Ele usa óculos, está com uma camisa verde escura e olhando para a lousa. A lousa está com 4 cálculos matemáticos diferentes e é iluminada por algumas luminárias próprias, presas no teto.",
+    alt: "Uma sala de aula cheia de alunos sentados em suas carteiras. Um professor, a frente da sala, está dando uma aula de matemática. Ele usa óculos, está com uma camisa verde escura e olhando para a lousa. A lousa está com 4 cálculos matemáticos e é iluminada por algumas luminárias próprias, presas no teto.",
   },
   {
     img: Img2,
-    alt: "Um conjunto de seis pessoas reunidas. Uma mulher loira, com um suéter vinho, sorrindo e usando óculos, está sentada em uma cadeira onde, a sua frente, há uma mesa com um notebook. Uma mulher morena, utilizando uma blusa azul com detalhes em branco e verde, está sorrindo e apontando apontando para o notebook. Um homem moreno, com barba e bigode, vestindo uma camisa social azul escuro, está sentado ao lado da mulher sentada, também aponta para o notebook e sorri. Há uma mulher negra, atrás da mulher sentada com o notebook, de pé. Ela tem tranças no cabelo e está com uma camiseta cinza claro. Ao lado dela, há outra mulher, morena, utilizando uma camisa sozial listrada de branco e azul. Ela está de pé e sorrindo. Ao seu lado, um homem de barba grisalha, sorrindo e comemorando, está utilizando uma camisa sozial cinza.",
+    alt: "Um conjunto de seis pessoas reunidas. Uma mulher loira, com um suéter vinho, sorrindo e usando óculos, está sentada em uma cadeira onde, a sua frente, há uma mesa com um notebook. Uma mulher morena de cabelos longos, utilizando uma blusa azul com detalhes em branco e verde, está sorrindo e apontando para o notebook. Um homem moreno, com barba e bigode, vestindo uma camisa social azul escuro, está sentado ao lado da mulher sentada, também aponta para o notebook e sorri. Há uma mulher negra, atrás da mulher sentada com o notebook, de pé. Ela tem tranças no cabelo e está com uma camiseta cinza claro. Ao lado dela, há outra mulher, morena, utilizando uma camisa social branca. Ela está de pé e sorrindo. Ao seu lado, um homem de barba grisalha e óculos, está sorrindo e comemorando, além de utilizar uma camisa sozial cinza.",
   },
   {
     img: Img3,
@@ -29,11 +28,7 @@ const images = [
   },
   {
     img: Img5,
-    alt: "Um grupo de pessoas em uma sala. Da direira para esquerda, há um homem sentado, de pernas cruzadas. Ele veste uma camisa social azul escura, com uma calça jeans vinho e um tênis preto com cadarços laranjas. Ao seu lado, há outro homem, de óculos, algumas tatuagens em seus braços e barba cheia preta. Ele está com uma camiseta branco com algumas listras pretas, um jeans azul e um tênis preto com amarelo. Ele também segura uma pequena pasta azul em suas mãos. Seguindo para a esquerda, há um homem moreno, com uma camiseta azul clara e um blazer azul escuro e jeans preto. Também há um detalhe amarelo seu blazer, na altura do peito. Logo em seguida, um mulher com a mão esquerda no rosto. Ela tem cabelos castanhos, utiliza uma blusa cinza clara e tem algumas pulseiras em seu braço. Na mão direta, ela segura um copo. Ao seu lado, uma mulher, que também parece ser o foco da sala, tem cabelos castanhos claros. Ela está com uma camisa preta e uma blusinha branca por cima. Também está com um colar longo preto.",
-  },
-  {
-    img: Img6,
-    alt: "Pessoas em seus notebooks digitando",
+    alt: "Um grupo de pessoas em uma sala. Da direira para esquerda, há um homem sentado, de pernas cruzadas. Ele veste uma camisa social azul escura, com uma calça jeans vinho e um tênis preto com cadarços laranjas. Ao seu lado, há outro homem, de óculos, algumas tatuagens em seus braços e barba cheia preta. Ele está com uma camiseta branco com algumas listras pretas, um jeans azul e um tênis preto com amarelo. Ele também segura uma pequena pasta azul em suas mãos. Seguindo para a esquerda, há um homem moreno, com uma camiseta azul clara e um blazer azul escuro e jeans preto. Também há um detalhe amarelo seu blazer, na altura do peito. Logo em seguida, um mulher com a mão esquerda no rosto. Ela tem cabelos castanhos, utiliza uma blusa cinza clara e tem algumas pulseiras em seu braço. Na mão direta, ela segura um copo. Ao seu lado, uma mulher, que também parece ser o foco da sala, tem cabelos castanhos claros. Ela está com uma camisa preta e uma blusinha branca por cima. Também está com um colar longo preto. AO lado, há um grupo de pessoas de lado para a imagem, onde todos estão com o foco na mulher que está se pronunciando naquele momento.",
   },
 ].sort(() => Math.random() - .5);
 
@@ -68,7 +63,7 @@ export function Carousel() {
       <S.Images id="carousel-items" aria-live="off">
         {images.map((image, i) => (
           <S.Image key={i} role="group" aria-roledescription="slide" active={i === index} aria-label={`Image ${i + 1} of ${images.length}`}>
-            <img src={image.img} alt={image.alt} loading="lazy" draggable="false" />
+            <img src={image.img} alt={image.alt} loading="lazy" draggable="false" width={2000} height={800} />
           </S.Image>
         ))}
       </S.Images>
