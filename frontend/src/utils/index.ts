39631c060836968c6de25f6bee19223bba2f0929
Carlos Faustino <chfcchfc96@gmail.com>
2022-03-22 16:49:41 -0300

    Função para mostrar o período por extenso

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,2 +1,10 @@
 export * from './alert';
 export * from './mask';
+
+export function showPeriod(period: 'M' | 'T' | 'N') {
+  return ({
+    'M': 'Manhã',
+    'T': 'Tarde',
+    'N': 'Noite',
+  })[period]
+}
